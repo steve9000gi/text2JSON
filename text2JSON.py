072b682d586635a98d66fc2da601281614415519
Steve Chall <stevec@renci.org>
2017-04-19 11:28:33 -0400

    fix truncated keys when no trailing ':'; replaced tabs

--- text2JSON.py
+++ text2JSON.py
@@ -2,10 +2,10 @@
 
 """ text2JSON.py: The single command line argument is the path to a text file of
         the format exported by clicking on the 'Save Text' button on
-	http://syssci.renci.org/sort/. The result is another file with the same
-	path except with the extension '.json' that contains a JSON object 
-	equivalent to what you would have gotten if you'd clicked on the 'Save
-	JSON' button. See https://github.com/steve9000gi/sort for details.
+        http://syssci.renci.org/sort/. The result is another file with the same
+        path except with the extension '.json' that contains a JSON object 
+        equivalent to what you would have gotten if you'd clicked on the 'Save
+        JSON' button. See https://github.com/steve9000gi/sort for details.
 """
 
 import sys
@@ -21,25 +21,25 @@ def generate_outfilename(infilename):
 def convert_list_to_dict(lst):
     """ Actual argument 'lst' is expected to be a sequence of strings 
             conceptually divided into subclusters by empty strings. The first 
-	    subcluster is expected not to be preceded by an empty string.  More
-	    than one empty string element in a row will cause an error. The
-	    first string in each subcluster is expected to have a colon as its
-	    final character, which is stripped off, after which that first
-	    string is used as the key in a key-value pair. The remaining strings
-	    in each subcluster are placed into a list which is used as the value
-	    in the key-value pair corresponding to that subcluster.
+            subcluster is expected not to be preceded by an empty string.  More
+            than one empty string element in a row will cause an error. The
+            first string in each subcluster is expected to have a colon as its
+            final character, which is stripped off, after which that first
+            string is used as the key in a key-value pair. The remaining strings
+            in each subcluster are placed into a list which is used as the value
+            in the key-value pair corresponding to that subcluster.
     """
     dct = {}
     i = 0
     lst_len = len(lst)
     while i < lst_len: 
         key = lst[i]
-        key = key[:-1]
+        key = key.strip(':')
         vals = []
         i += 1
         while len(lst[i]) != 0:
             vals.append(lst[i])
-	    i += 1
+            i += 1
         dct[key] = vals
         i += 1
     return dct
